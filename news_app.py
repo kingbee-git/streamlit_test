c0ba312a1d974e2094ebd60bf7fbba9280ca1b9e
kingbee-git <kingbeebmk2@gmail.com>
2024-06-27 09:58:25 +0900

    updated

--- news_app.py
+++ news_app.py
@@ -6,6 +6,9 @@ import pandas as pd
 import utils
 
 
+def make_clickable(val):
+    return f'<a target="_blank" href="{val}">{val}</a>'
+
 def news_app():
     news_df_yesterday, news_df_today = utils.load_news_data()
 
@@ -17,7 +20,19 @@ def news_app():
 
     st.subheader("키워드 내 뉴스 입니다.")
     st.markdown("<h5>새로운 뉴스 입니다.</h5>", unsafe_allow_html=True)
-    st.dataframe(news_new)
+    st.data_editor(
+        news_new[['기사날짜', '제목', 'URL', '내용']],
+        column_config={
+            "URL": st.column_config.LinkColumn(
+                "URL",
+                display_text="기사 보기",
+                help="기사의 원본 링크",
+                validate="^https?://",
+                max_chars=100
+            )
+        },
+        hide_index=True,
+    )
 
     st.markdown("<h5>키워드 내 뉴스 입니다.</h5>", unsafe_allow_html=True)
     news_column_index = news_df_today.columns.get_loc(news_key_column)
@@ -27,7 +42,33 @@ def news_app():
     if news_search_term:
         news_filtered_df = news_df_today[
             news_df_today[news_column].str.contains(news_search_term, case=False, na=False)]
-        st.dataframe(news_filtered_df)
+
+        st.data_editor(
+            news_filtered_df[['기사날짜', '제목', 'URL', '내용']],
+            column_config={
+                "URL": st.column_config.LinkColumn(
+                    "기사 URL",
+                    display_text="기사 보기",
+                    help="기사의 원본 링크",
+                    validate="^https?://",
+                    max_chars=100
+                )
+            },
+            hide_index=True,
+        )
     else:
         news_filtered_df = news_df_today
-        st.dataframe(news_filtered_df)
+
+        st.data_editor(
+            news_filtered_df[['기사날짜', '제목', 'URL', '내용']],
+            column_config={
+                "URL": st.column_config.LinkColumn(
+                    "URL",
+                    display_text="기사 보기",
+                    help="기사의 원본 링크",
+                    validate="^https?://",
+                    max_chars=100
+                )
+            },
+            hide_index=True,
+        )
