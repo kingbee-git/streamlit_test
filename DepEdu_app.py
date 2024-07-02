bec1ff643d86b2efe38b1eb858e80d882abc45ff
kingbee-git <kingbeebmk2@gmail.com>
2024-07-02 14:28:05 +0900

    updated

--- DepEdu_app.py
+++ DepEdu_app.py
@@ -13,7 +13,12 @@ def DepEdu_app():
     dep_edu_key_column = '과업명'
     numeric_columns = ['금액']
 
-    dep_edu_column_index = dep_edu_df.columns.get_loc(numeric_columns)
+    for column in numeric_columns:
+        if column in dep_edu_df.columns:
+            dep_edu_df[column] = dep_edu_df[column].str.replace(',', '').astype(float)
+
+
+    dep_edu_column_index = dep_edu_df.columns.get_loc(dep_edu_key_column)
     dep_edu_column = st.selectbox('필터링할 열 선택', dep_edu_df.columns, index=dep_edu_column_index,
                                     key='dep_edu_key_column')
 
@@ -21,11 +26,11 @@ def DepEdu_app():
         min_value = float(dep_edu_df[dep_edu_column].min())
         max_value = float(dep_edu_df[dep_edu_column].max())
         dep_edu_range = st.slider(f'{dep_edu_column}에서 검색할 범위 선택', min_value=min_value, max_value=max_value,
-                                    value=(min_value, max_value), key='QWGJK_new_range')
+                                    value=(min_value, max_value), key='dep_edu_range')
         dep_edu_filtered_df = dep_edu_df[
             (dep_edu_df[dep_edu_column] >= dep_edu_range[0]) & (dep_edu_df[dep_edu_column] <= dep_edu_range[1])]
     else:
-        dep_edu_search_term = st.text_input(f'{dep_edu_column}에서 검색할 내용 입력', key='QWGJK_new_search_term')
+        dep_edu_search_term = st.text_input(f'{dep_edu_column}에서 검색할 내용 입력', key='dep_edu_search_term')
         if dep_edu_search_term:
             dep_edu_filtered_df = dep_edu_df[
                 dep_edu_df[dep_edu_column].str.contains(dep_edu_search_term, case=False, na=False)]
