6269b04af646566611353770e46683b2d1dc686a
kingbee-git <kingbeebmk2@gmail.com>
2024-07-05 09:16:27 +0900

    updated

--- app.py
+++ app.py
@@ -100,8 +100,8 @@ def main():
                 "nav-link-selected": {"background-color": "#02ab21", "color": "#fff"},
             }
 
-            selected = option_menu("Mido Plus", ["현황", "예산 및 사업 맵핑 테스트 중", "지자체 예산서", "교육청 예산서", "인포21C", "종합쇼핑몰 납품상세 내역", "뉴스", "orderlist", "orderlist_realtime", "orderlist_updated", "STAT"],
-                                   icons=["house", "gear", "gear", "gear", "gear", "gear", "gear", "gear", "gear", "gear", "gear"],
+            selected = option_menu("Mido Plus", ["납품 현황", "사업 현황", "지자체 예산서", "교육청 예산서", "인포21C", "종합쇼핑몰 납품상세 내역", "뉴스", "orderlist", "orderlist_realtime", "orderlist_updated", "STAT"],
+                                   icons=["graph-up-arrow", "list-check", "building", "building", "info-square", "cart4", "pencil-square", "gear", "gear", "gear", "clipboard-data"],
                                    menu_icon="cast",
                                    default_index=0,
                                    orientation="vertical",
@@ -109,10 +109,10 @@ def main():
                                    styles=styles,
                                    )
 
-        if selected == "현황":
+        if selected == "납품 현황":
             utils.log_user_action(st.session_state['username'], "viewed HOME", "mido_test", "logs")
             home_app.home_app()
-        elif selected == "예산 및 사업 맵핑 테스트 중":
+        elif selected == "사업 현황":
             utils.log_user_action(st.session_state['username'], "viewed list", "mido_test", "logs")
             listup_app.listup_app()
         elif selected == "지자체 예산서":
